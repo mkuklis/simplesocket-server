92188a6a2465d93952023d74088f487ff16bb14f
Michal Kuklis <michal.kuklis@gmail.com>
2013-10-17 01:26:33 +0200

    Fixed formmating.

--- test/server_test.js
+++ test/server_test.js
@@ -4,18 +4,18 @@ var port = process.env.PORT || 5000;
 var server = app.listen(port);
 
 var ss = require('../index').listen(server, {
-	authorize: authorize
+  authorize: authorize
 });
 
 ss.on('connection', function (socket) {
-	var roomNo = Math.floor(Math.random(10) * 10);
+  var roomNo = Math.floor(Math.random(10) * 10);
 
-	var time = Math.floor(Math.random() * 2000) + 1000;
-	socket.join('room' + roomNo);
-	
-	setInterval(function () {
-		ss.in('room' + roomNo).emit('hello', 'room' + roomNo);
-	}, time); 
+  var time = Math.floor(Math.random() * 2000) + 1000;
+  socket.join('room' + roomNo);
+  
+  setInterval(function () {
+    ss.in('room' + roomNo).emit('hello', 'room' + roomNo);
+  }, time); 
 
 });
 
@@ -24,5 +24,5 @@ ss.on('close', function (socket) {
 });
 
 function authorize(info, callback) {
-	callback(true);
+  callback(true);
 }
